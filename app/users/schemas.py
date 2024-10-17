d434d11cc7a06f8bef7a5b8316fc99db703cb88c
M1nce1337 <144385055+M1nce1337@users.noreply.github.com>
2024-10-17 23:17:13 +0300

    Added comments

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,17 +1,17 @@
 from pydantic import BaseModel, EmailStr, Field
 
 
-class SUserRegister(BaseModel):
+class SUserRegister(BaseModel): # модель данных для регистрации пользователя
     email: EmailStr = Field(..., description="Электронная почта")
     password: str = Field(..., min_length=5, max_length=50, description="Пароль, от 5 до 50 знаков")
     password_check: str = Field(..., min_length=5, max_length=50, description="Пароль, от 5 до 50 знаков")
     name: str = Field(..., min_length=3, max_length=50, description="Имя, от 3 до 50 символов")
 
 
-class SUserAuth(BaseModel):
+class SUserAuth(BaseModel): # модель данных для авторизации пользователя
     email: EmailStr = Field(..., description="Электронная почта")
     password: str = Field(..., min_length=5, max_length=50, description="Пароль, от 5 до 50 знаков")
 
-class Token(BaseModel):
+class Token(BaseModel): # модель токена доступа
     access_token: str
     token_type: str
