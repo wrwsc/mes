f39a9746082261b477c29650b095dcda09ffb492
M1nce1337 <144385055+M1nce1337@users.noreply.github.com>
2024-10-17 22:25:01 +0300

    Added comments

--- app/chat/models.py
+++ app/chat/models.py
@@ -2,8 +2,16 @@ from sqlalchemy import Integer, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
+"""
+() Создаёт модель сообщения для базы данных.
+() В данной модели определены следующие поля:
+() id: уникальный идентификатор сообщения
+() sender_id: идентификатор отправителя сообщений, связанный с таблицей пользователей
+() recipient_id: идентификатор получателя сообщения, также связанный с таблицей пользователей
+() content: текст сообщения
+"""
 
-class Message(Base):
+class Message(Base): 
     __tablename__ = 'messages'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
